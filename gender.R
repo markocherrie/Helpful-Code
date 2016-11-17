0d280be1de93e26ee9e856dd9f56a28ea8bd5a11
Mark <mark.cherrie.mc@gmail.com>
2016-11-17 11:19:11 +0000

    Update gender.R

--- gender.R
+++ gender.R
@@ -1,4 +1,4 @@
-#install.packages('googleViz')
+install.packages('googleViz')
 setwd("C:/Users/mcherrie/Google Drive/gender")
 library(genderizeR)
 library(googleViz)
@@ -66,42 +66,3 @@ doughnut=gvisPieChart(dat,
                       chartid="doughnut")
 plot(doughnut)
 
-library("rvest")
-library(stringr)
-library(dplyr)
-library(genderizeR)
-library(ggplot2)
-library("googleVis")
-library("XML")
-paste0("http://www.crn.com/slide-shows/data-center/300076704/2015-big-data-100-business-analytics.htm/pgno/0/", 1:45) %>%
-  c(., paste0("http://www.crn.com/slide-shows/data-center/300076709/2015-big-data-100-data-management.htm/pgno/0/",1:30)) %>%
-  c(., paste0("http://www.crn.com/slide-shows/data-center/300076740/2015-big-data-100-infrastructure-tools-and-services.htm/pgno/0/",1:25)) -> webpages
-results=data.frame()
-for(x in webpages)
-{
-  read_html(x) %>% html_nodes("p:nth-child(1)") %>% .[[2]] %>% html_text() -> Company
-  read_html(x) %>% html_nodes("p:nth-child(2)") %>% .[[1]] %>% html_text() -> Executive
-  results=rbind(results, data.frame(Company, Executive))
-}
-results=data.frame(lapply(results, as.character), stringsAsFactors=FALSE)
-results[24,]=c("Trifacta", "Top Executive: CEO Adam Wilson")
-results %>% mutate(Name=gsub("Top|\bExec\S*|\bCEO\S*|President|Founder|and|Co-Founder|\:", "", Executive)) %>%
-  mutate(Name=word(str_trim(Name))) -> results
-results %>%
-  select(Name) %>%
-  findGivenNames() %>%
-  filter(probability > 0.9 & count > 15) %>%
-  as.data.frame() -> data
-data %>% group_by(gender) %>% summarize(Total=n()) -> dat
-doughnut=gvisPieChart(dat,
-                      options=list(
-                        width=450,
-                        height=450,
-                        legend="{ position: 'bottom', textStyle: {fontSize: 10}}",
-                        chartArea="{left:25,top:50}",
-                        title='TOP 100 BIG DATA COMPANIES 2015
-                        Gender of CEOs',
-                        colors="['red','blue']",
-                        pieHole=0.5),
-                      chartid="doughnut")
-plot(doughnut)
