f46b565770db1404a1376f659660a45e7cff2df7
Mark <mark.cherrie.mc@gmail.com>
2016-12-16 15:19:43 +0000

    Create all_combos.R

---
+++ all_combos.R
@@ -0,0 +1,8 @@
+df1 <- c("a","b","c")
+df2 <- c("1","2","3")
+
+df3<-expand.grid(df1,df2)
+df3$Var3<-paste0(df3$Var1,df3$Var2)
+paste(df3$Var3, collapse=" ")
+
+apply(expand.grid(df1, df2), 1, paste, collapse="")
