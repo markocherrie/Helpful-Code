8d9bca747b9e55221d7fffe60828a7c545370333
Mark <mark.cherrie.mc@gmail.com>
2017-01-06 10:18:46 +0000

    Create commentblock.R

---
+++ commentblock.R
@@ -0,0 +1,13 @@
+# Useful for keeping code clean
+# Instead of commenting out everyline
+# Wrap the commands within a function with no argument as below:
+# http://stackoverflow.com/questions/9107695/r-comment-out-block-of-code
+
+
+
+if(FALSE) {
+# VIZ CODE NOT IMPORTANT FOR MAIN ANALYSES
+}
+
+
+# Could come in handy if you have meaty vizualisations that you don't want to be plotted everytime you run the script
