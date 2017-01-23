ffa01f39b1b3dacc53a8490057441e1c8d4e8a30
Mark <mark.cherrie.mc@gmail.com>
2017-01-23 15:29:16 +0000

    Update Indexing.R

--- Indexing.R
+++ Indexing.R
@@ -1,9 +1,13 @@
 ## Indexing
 
-## [col, row]
+## [row, col]
 ## If you want more than one column not in order then use c(,):
-grp_cols <- names(df)[c(1,4)] 
+x <- names(x)[c(1,4)] 
 
 
 ## drop if duplicated in one column
 x<-x[!duplicated(x[,1]),]
+
+
+## drop if missing in one column
+x<-x[!(is.na(x[,1)
