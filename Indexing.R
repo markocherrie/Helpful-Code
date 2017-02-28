47ef08d946d5fd73ae8d5b98b1fec9b9c5556a73
Mark <mark.cherrie.mc@gmail.com>
2017-02-28 20:22:13 +0000

    Update Indexing.R

--- Indexing.R
+++ Indexing.R
@@ -4,6 +4,9 @@
 ## If you want more than one column not in order then use c(,):
 x <- names(x)[c(1,4)] 
 
+# renaming single column
+colnames(data)[2] <- "newname"
+
 
 ## drop if duplicated in one column
 x<-x[!duplicated(x[,1]),]
