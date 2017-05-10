e89d3ee33e9e9679874e7c4f2ccb90c92818057e
Mark Cherrie <mark.cherrie.mc@gmail.com>
2017-05-10 19:37:07 +0100

    Initial Commit

---
+++ SafeHavenRPackages.R
@@ -0,0 +1,19 @@
+#### RUN THIS ON YOUR OWN COMPUTER
+packagesstring<- installed.packages()[, "Package"]
+saveRDS(packagesstring, "packagestring.RDS")
+
+
+#### TAKE .RDS FILE TO SAFEHAVEN, PUT IT IN WORKING DIRECTORY
+
+SAFEHAVENinstall<-function(nopackagesinsafehaven){
+if (nopackagesinsafehaven=T){
+packagesstring<-readRDS("packagestring.RDS")
+install.packages(packagesstring)
+if else(nopackagesinsafehaven=F)
+list.of.packages<-installed.packages()[, "Package"]
+new.packages<- packagesstring[!(packagesstring %in% list.of.packages)]
+if(length(new.packages)>0) install.packages(new.packages)
+else(
+  print("Has to be true (T) or false (F)")
+)
+}
