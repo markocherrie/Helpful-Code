3a7d839b44492d0f189313847521c059051cb061
Mark <mark.cherrie.mc@gmail.com>
2017-05-23 11:33:49 +0100

    Update chisquare.R

--- chisquare.R
+++ chisquare.R
@@ -1,4 +1,4 @@
-## descriptive stats
+## handy for papers, to get an immediate chi suqare test in the difference of two proportions
 CHI<-function(pop1,number1, pop2, number2){
 data = matrix(c(number1,number2,(pop1-number1),(pop2-number2)),ncol=2)
 chisq.test(data,correct=F)
