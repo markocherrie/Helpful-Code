278d91094ebf16fe33dec369d8e3c16dc23f3a4f
Mark <mark.cherrie.mc@gmail.com>
2017-11-29 13:29:24 +0000

    Update panelfillNA.R

--- panelfillNA.R
+++ panelfillNA.R
@@ -1,5 +1,13 @@
 # If you need to fillin the next observation with the previous ones, 
 # for each paticipant, this piece of code which uses the zoo package is handy
 
+
+# Here we say, for each observation carry it onto the next time point for each participant
+# i.e. Last Observation Carried Forward
 df2<-do.call(rbind, by(df, df$Participant_ID, na.locf))
 
+# Now with the new dataset we might say that if they haven't specified a variable then when they did 
+# take that and use if for the previous time points, for each participant
+# i.e. Next Observation Carried Backward
+
+df2<-do.call(rbind, by(df2, df2$Participant_ID, function(x) na.locf(x, fromLast = T)))
