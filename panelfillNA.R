c8e9ae182957faf82ae52e6388b13d5fe99f3b61
Mark <mark.cherrie.mc@gmail.com>
2017-11-29 16:58:19 +0000

    Update panelfillNA.R

--- panelfillNA.R
+++ panelfillNA.R
@@ -1,4 +1,4 @@
-# If you need to fillin the next observation with the previous ones, 
+# If you need to fill in the next observation with the previous ones, 
 # for each paticipant, this piece of code which uses the zoo package is handy
 
 
