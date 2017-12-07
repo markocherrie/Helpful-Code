0e7e316fa3f698ec003eec06da3f7184a2abd0ff
Mark <mark.cherrie.mc@gmail.com>
2017-12-07 15:39:34 +0000

    Update splitvartomore.R

--- splitvartomore.R
+++ splitvartomore.R
@@ -3,6 +3,22 @@
 
 df<-NULL
 df$greetingnamelocation<-"hello_marko_Turin"
-df$greeting <- sapply(strsplit(as.character(df$Travel_ID),"_"),function(x) x[1])
-df$name <- sapply(strsplit(as.character(df$Travel_ID),"_"),function(x) x[2])
-df$location <- sapply(strsplit(as.character(df$Travel_ID),"_"),function(x) x[3])
+df$greeting <- sapply(strsplit(as.character(df$greetingnamelocation),"_"),function(x) x[1])
+df$name <- sapply(strsplit(as.character(df$greetingnamelocation),"_"),function(x) x[2])
+df$location <- sapply(strsplit(as.character(df$greetingnamelocation),"_"),function(x) x[3])
+                      
+                      
+#### Or if you want to do this for lots of csvs
+
+formatcsvs <- function(path){
+df<-read.csv(path)
+df$greeting <- sapply(strsplit(as.character(df$greetingnamelocation),"_"),function(x) x[1])
+df$name <- sapply(strsplit(as.character(df$greetingnamelocation),"_"),function(x) x[2])
+df$location <- sapply(strsplit(as.character(df$greetingnamelocation),"_"),function(x) x[3])
+# subset based on new columns
+df<-df[,c(1,6,7,2,4)]
+write.csv(df, path, row.names=F)
+}
+
+filenames=list.files(path="test", "*csv", full.names=TRUE)
+mapply(formatcsvs, path = filenames)
