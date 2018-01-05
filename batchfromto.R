250a75115765bdd5f6959245a19b09983f6bc5ce
Mark <mark.cherrie.mc@gmail.com>
2018-01-05 18:19:29 +0000

    Create batchfromto.R

---
+++ batchfromto.R
@@ -0,0 +1,9 @@
+# say you want to iterate over a number of files (n=3971 in the example), but you want to do it in 300 file batches
+# this will split your files so you can then run the function via ddply
+
+input<-0:3971
+input2<-input[(input %% (300)) == 0]+1
+input3<-input2+299
+input3[length(input3)]<-max(input)
+input4<-data.frame(filebatchfrom=input2, filebatchto=input3)
+
