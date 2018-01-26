80d753e9bf132fd03cd7c3912bd70fd57f6405d3
Mark <mark.cherrie.mc@gmail.com>
2018-01-26 14:38:44 +0000

    Update and rename n-dash.R to En-dash.R

--- En-dash.R
+++ En-dash.R
@@ -2,7 +2,7 @@
 
 # the reason might be that you have an n-dash instead of a hyphen
 
-# Put the correct code for the n-dash and it will work
+# Put the correct code for the En-dash and it will work
 
 # i.e. this:
 data$wave[finaldata2$Site=='9 \u2013 Canary Wharf'& (data$date>='2017-10-16'& data$date<='2017-11-03')]<-3  
