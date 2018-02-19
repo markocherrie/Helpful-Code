809a4611b9f582aabf474f52c7736f3f2fa218cd
Mark <mark.cherrie.mc@gmail.com>
2018-02-19 15:53:02 +0000

    Update Indexing.R

--- Indexing.R
+++ Indexing.R
@@ -21,7 +21,7 @@ cols <- c(1, 4:8, 10)
 x<-x[,cols]
 
 # select columns based on regular expression
-x<-x[grep("^D", x$y), ]
+x<-x[grep("^D", colnames(x), ]
 
 # to get every nth row (example=3rd)
 x<-x[seq(1, nrow(x), 3), ]
